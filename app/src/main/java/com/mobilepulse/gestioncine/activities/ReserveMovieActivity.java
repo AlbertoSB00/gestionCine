fda895c50bb880eb23794e529b2dc2ac9f3dab9c
AlbertoSB00 <sanchezbaronaalberto560@gmail.com>
2024-05-31 13:06:58 +0200

    logica para implementar las reservas

--- app/src/main/java/com/mobilepulse/gestioncine/activities/ReserveMovieActivity.java
+++ app/src/main/java/com/mobilepulse/gestioncine/activities/ReserveMovieActivity.java
@@ -3,8 +3,11 @@ package com.mobilepulse.gestioncine.activities;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +16,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.mobilepulse.gestioncine.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ReserveMovieActivity extends AppCompatActivity {
+
+    private static final String IP = "192.168.0.108";
+    private static final int PORT = 12345;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(2);
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     private Spinner spinnerSala;
     private Spinner spinnerHorario;
@@ -24,21 +42,36 @@ public class ReserveMovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve_movie);
 
+        spinnerSala = findViewById(R.id.spinnerSala);
+        spinnerHorario = findViewById(R.id.spinnerHorario);
 
         // Obtener el titulo de la película.
         String titulo = getIntent().getStringExtra("titulo");
         String correo = getIntent().getStringExtra("correo");
-        Toast.makeText(this, correo, Toast.LENGTH_SHORT).show();
         TextView textView = findViewById(R.id.titulo);
         textView.setText(titulo);
 
         // Al pulsar el botón "Proceder con el pago".
         Button buttonConfirmar = findViewById(R.id.buttonConfirmar);
         buttonConfirmar.setOnClickListener(v -> {
+            // Obtener el estado de la reserva
+            String estadoReserva = "Confirmada";
 
-            // Antes de continuar con el pago, hacemos las gestiones para crear la reserva en la bd.
+            // Obtener el ID de usuario y de película de manera asíncrona
+            CompletableFuture<Integer>[] futures = new CompletableFuture[2];
+            futures[0] = obtenerIdUsuario(correo);
+            futures[1] = obtenerIdPelicula(titulo);
 
+            // Cuando se completen ambos futures, realizar la reserva
+            CompletableFuture.allOf(futures).thenAcceptAsync(result -> {
+                try {
+                    reservar(futures[0].get(), futures[1].get(), spinnerSala.getSelectedItemId(), spinnerHorario.getSelectedItemId(), estadoReserva, contarButacasReservadas());
+                } catch (Exception e) {
+                    mostrarMensaje("Error al realizar la reserva: " + e.getMessage());
+                }
+            });
         });
+
     }
 
     public void reservarButaca(View view) {
@@ -53,5 +86,128 @@ public class ReserveMovieActivity extends AppCompatActivity {
             // Si está reservada, cambia a libre
             butaca.setBackgroundResource(R.drawable.butaca_libre);
         }
+    }
+
+    private int contarButacasReservadas() {
+        int count = 0;
+
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            View child = gridLayout.getChildAt(i);
+            if (child instanceof Button) {
+                Button button = (Button) child;
+                Drawable background = button.getBackground();
+                Drawable reservedDrawable = getResources().getDrawable(R.drawable.butaca_ocupada);
+
+                if (background.getConstantState().equals(reservedDrawable.getConstantState())) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+    private CompletableFuture<Integer> obtenerIdUsuario(String correo) {
+        return CompletableFuture.supplyAsync(() -> {
+            try (Socket socket = new Socket(IP, PORT);
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+                // Enviamos orden al servidor.
+                out.println("GET_USER_ID");
+
+                // Enviamos el correo.
+                out.println(correo);
+
+                // Leemos respuesta.
+                String response = in.readLine();
+                return Integer.parseInt(response);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error al obtener el ID de usuario.");
+            }
+        }, executorService);
+    }
+
+    private CompletableFuture<Integer> obtenerIdPelicula(String titulo) {
+        return CompletableFuture.supplyAsync(() -> {
+            try (Socket socket = new Socket(IP, PORT);
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+                // Enviamos orden al servidor.
+                out.println("GET_MOVIE_ID");
+
+                // Enviamos el titulo.
+                out.println(titulo);
+
+                // Leemos respuesta.
+                String response = in.readLine();
+                return Integer.parseInt(response);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error al obtener el ID de película.");
+            }
+        }, executorService);
+    }
+
+    private void reservar(int idUsuario, int idPelicula, Object idSala, Object idProyeccion, String estadoReserva, int butacasReservadas) {
+        executorService.execute(() -> {
+            String response = authenticationTask(idUsuario, idPelicula, idSala, idProyeccion, estadoReserva, butacasReservadas);;
+
+            runOnUiThread(() -> {
+                switch (response) {
+                    case "INSERT_MOVIE_SUCCESS":
+                        Toast.makeText(this, "Reserva realizada con exito", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case "INSERT_MOVIE_FAILED":
+                        Toast.makeText(this, "Error al realizar la reserva", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            });
+        });
+    }
+
+    private String authenticationTask(int idUsuario, int idPelicula, Object idSala, Object idProyeccion, String estadoReserva, int butacasReservadas) {
+        String response = "";
+        try {
+            Socket socket = new Socket(IP, PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Enviamos orden al servidor.
+            out.println("INSERT_RESERVE");
+
+            // Enviamos datos al servidor.
+            out.println(idUsuario);
+            out.println(idPelicula);
+            out.println(idSala);
+            out.println(idProyeccion);
+            out.println(estadoReserva);
+            out.println(butacasReservadas);
+
+            // Leemos la respuesta.
+            response = in.readLine();
+
+            // Cerramos el socket.
+            out.close();
+            in.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        handler.post(() -> Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show());
     }
 }
